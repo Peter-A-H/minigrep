e3905cfedd4609dfe3d4e9d96634f9f6742b38b4
Peter <peterhamblin@hotmail.co.uk>
2024-05-07 21:48:21 +0100

    Refactor parse function to implement method on Config struct

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     let file_contents =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{file_contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
